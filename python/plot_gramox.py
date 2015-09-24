3746d0602de01a79624e559d4d208e3ac2b806bb
Akshay Paropkari <paropkari.1@gmail.com>
2015-09-24 13:45:50 -0400

    Added plot_gramox.py

---
+++ python/plot_gramox.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Abstract: Plot categorized gramox data. Currently, this script plots only 4
+          data groups.
+
+Date: 09/24/2015
+
+Author: Akshay Paropkari
+"""
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+# from collections import defaultdict
+
+
+def handle_program_options():
+    parser = argparse.ArgumentParser(
+        description='Plot categorized gramox data.')
+    parser.add_argument('gramox_fnh',
+                        help='File with categorized gramox count data.')
+    return parser.parse_args()
+
+
+def main():
+    args = handle_program_options()
+
+    try:
+        with open(args.gramox_fnh):
+            pass
+    except IOError as ioe:
+        err_msg = '\nError opening gramox count file: {}\n'
+        sys.exit(err_msg.format(ioe))
+
+# Read gramox data
+    cat_gramox_data = pd.read_csv(args.gramox_fnh, sep='\t')
+    gdata = np.array(cat_gramox_data)
+
+# Data to plot
+#     conditions = [' '.join(cond.split('_')) for cond in np.unique(gdata[:,0])]
+#     categories = [' '.join(cat.split('_')) for cat in np.unique(gdata[:,1])]
+    conditions = np.unique(gdata[:,0])
+    categories = np.unique(gdata[:,1])
+    print 'conditions: ', conditions
+    print 'categories: ', categories
+    n_groups = len(conditions)
+    width = (1 - 0.3) / (len(conditions))
+    colors = ['#f1b6da', '#d01c8b', '#b8e186', '#4dac26']
+
+# Create plot
+    fig = plt.figure(figsize={30, 15})
+    ax = fig.add_subplot(111)
+    for i,cond in enumerate(conditions):
+        ind = range(1, len(categories)+1)
+        vals = gdata[gdata[:,0] == cond][:,2].astype(np.float)
+        pos = [j - (1 - 0.3) / 2. + i * width for j in ind]
+        ax.bar(pos, vals, width=width, label=cond, color=colors[i])
+        for x, y in zip(pos, vals):
+            ax.text(x+0.05, y+1, int(y), fontsize=20)
+    ax.set_ylabel("Counts", size=24)
+    ax.set_ylim(top=max(gdata[:, 2])+15)
+    ax.set_xticks(ind)
+    ax.set_xticklabels(categories, size=22)
+    ax.legend(loc='best', fontsize=22)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
