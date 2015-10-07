6f2ea88552b2d8c781be0cbf220cfcef6984e35f
Akshay Paropkari <paropkari.1@gmail.com>
2015-10-07 16:06:07 -0400

    Added trim_three_prime_end_adapters.py

---
+++ python/trim_three_prime_end_adapters.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+'''
+Abstract: Iterate through all folders and perform adapter trimming on all
+          reads. This script uses a sequencing mapping file to map wells to
+          sampleids and trim 3'-end adapters of reads. Additionally, reads with
+          lengths <200 nt are discarded. For more information on Skewer, visit
+          http://www.biomedcentral.com/1471-2105/15/182
+
+Date: 10/7/2015
+
+Author: Akshay Paropkari
+'''
+
+import os
+import sys
+import argparse
+import subprocess as sp
+from csv import DictReader
+try:
+    import shlex
+except ImportError as ie:
+    sys.exit('Please install {} module before executing this script.'
+             .format(ie))
+
+
+def prog_options():
+    parser = argparse.ArgumentParser(
+                description='Iterate through each folder in a file and trim '
+                            '3\'-end adapters sample fastq files via Skewer. '
+                            'Using adapter-to-primer mapping (adapters) and '
+                            'well-to-sampleid mapping (ws_map), trim command '
+                            'and output files are generated. This scripts '
+                            'discards reads with length <200 nt.')
+    parser.add_argument('sample_dir',
+                        help='Directory containing sample folders.')
+    parser.add_argument('ap_map',
+                        help='Path to tab-delimited file containing '
+                             'adapter-primer pair per line. Reverse primer '
+                             'adapters must be reverse complemented entries.')
+    parser.add_argument('ws_map',
+                        help='Path to tab-separated sample ID to adapter '
+                             'mapping file. A "-" must be used for empty '
+                             'cells.Please note that reverse adapters must be '
+                             'reverse complemented wherever necessary. Format '
+                             'of the file: well->sampleid->adapters.')
+    parser.add_argument('-t', '--threads', type=int, default=4,
+                        help='Optionally, you may specify number of threads to'
+                             ' be used for trimming. Default is 4.')
+    return parser.parse_args()
+
+
+def main():
+    args = prog_options()
+
+# Checking the necessary input arguments for validity.
+    try:
+        with open(args.ap_map):
+            pass
+    except IOError as ioe:
+        sys.exit('\nError with adapter file: {}\n'.format(ioe))
+
+    try:
+        with open(args.ws_map):
+            pass
+    except IOError as ioe:
+        sys.exit('\nError with the well-to-sampleid mapping file: {}\n'
+                 .format(ioe))
+
+# Iterate through all directories and access their files
+    for root, dirs, files in os.walk(args.sample_dir):
+        if root != args.sample_dir:
+            os.chdir(root)
+            well = root[24:27]
+            print '\n', well
+
+# Read adapter sequences from file
+            with open(args.ap_map, 'rU') as apf:
+                adapters = {line.strip().split('\t')[0]: line.strip().split('\t')[1]
+                            for line in apf.readlines()[1:]}
+
+# Read mapping file data
+            with open(args.ws_map, 'rU') as csvf:
+                map_data = [line
+                            for line in DictReader(csvf, delimiter='\t')]
+
+# Calculate skewer command for each sample
+            for sample in map_data:
+                if sample['well'] == well:
+                    fname1 = sample['P1_sample']+'_27F-assigned-01.fastq'
+                    fname2 = sample['P1_sample']+'_515F-assigned-01.fastq'
+                    fname3 = sample['P2_sample']+'_27F-assigned-01.fastq'
+                    fname4 = sample['P2_sample']+'_515F-assigned-01.fastq'
+
+                    # Plate 1 trimming
+                    for file in files:
+                        if file == fname1:
+                            cmd1 = 'skewer -t {} -b -m tail -l 200 -x {} \
+                                   -o {}_519R {}'.format(args.threads,
+                                                         sample['519R_P1'],
+                                                         file.split('_')[0],
+                                                         file)            # P1
+                            kwargs1 = shlex.split(cmd1)
+                            print 'Well: {} | Primer: {} | SampleID: {}'\
+                                  .format(sample['well'],
+                                          adapters[sample['519R_P1']],
+                                          file.split('_')[0])            # P1
+                            print kwargs1, '\n'
+                            out1 = sp.check_output(kwargs1)
+                            print out1
+
+                        elif file == fname2:
+                            cmd2 = 'skewer -t {} -b -m tail -l 200 -x {} \
+                                   -o {}_806R {}'.format(args.threads,
+                                                         sample['806R_P1'],
+                                                         file.split('_')[0],
+                                                         file)            # P1
+                            kwargs2 = shlex.split(cmd2)
+                            print 'Well: {} | SampleID: {} | Primer: {}'\
+                                  .format(sample['well'],
+                                          adapters[sample['806R_P1']],
+                                          file.split('_')[0])             # P1
+                            print kwargs2, '\n'
+                            out2 = sp.check_output(kwargs2)
+                            print out2
+
+                    # Plate 2 trimming
+                        elif file == fname3:
+                            if sample['519R_P2'] != '-':
+                                cmd3 = 'skewer -t {} -b -m tail -l 200 -x {} \
+                                       -o {}_519R {}'\
+                                       .format(args.threads, sample['519R_P2'],
+                                               file.split('_')[0], file)  # P2
+                                kwargs3 = shlex.split(cmd3)
+                                print 'Well: {} | Primer: {} | SampleID: {}'\
+                                      .format(sample['well'],
+                                              adapters[sample['519R_P2']],
+                                              file.split('_')[0])         # P2
+                                print kwargs3, '\n'
+                                out3 = sp.check_output(kwargs3)
+                                print out3
+                        elif file == fname4:
+                            if sample['806R_P2'] != '-':
+                                cmd4 = 'skewer -t {} -b -m tail -l 200 -x {} \
+                                       -o {}_806R {}'\
+                                       .format(args.threads, sample['806R_P2'],
+                                               file.split('_')[0], file)  # P2
+                                kwargs4 = shlex.split(cmd4)
+                                print 'Well: {} | Primer: {} | SampleID: {}'\
+                                      .format(sample['well'],
+                                              adapters[sample['806R_P2']],
+                                              file.split('_')[0])         # P2
+                                print kwargs4, '\n'
+                                out4 = sp.check_output(kwargs4)
+                                print out4
+    return
+
+if __name__ == '__main__':
+    sys.exit(main())
