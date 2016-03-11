7c15e566fdbadd123ff6f06c7057c5d29e7ba7b4
Akshay Paropkari <paropkari.1@gmail.com>
2016-03-11 16:28:46 -0500

    Updated pct_abd_gramox.py. Plotting abundance percentage by groups instead of gramox status for easier interpretation. Legend colors for gramox status are colorblind safe and printer friendly. Annotation of bars with percent abundance value is optional now. Calculation data, for per sample and per category, of percent abundance will be written to file. Intermediate calculations writing to file is optional (useful for debugging purposes).

--- python/pct_abd_gramox.py
+++ python/pct_abd_gramox.py
@@ -75,21 +75,33 @@ def calc_rel_abd(biomf, sampleIDs=None):
 
 def handle_program_options():
     parser = argparse.ArgumentParser(description="Calculate categorized gramox"
-                                     " data and plot it.")
+                                     " data and plot it. Script will also have"
+                                     " the option to write out data files (see"
+                                     " cat_gramox_out, sample_gramox_out and "
+                                     "-c options).")
     parser.add_argument("gramox_fnh",
                         help="File with gramox data for each OTU and 'NA' if "
                              " data is unavailable or inadequate.")
     parser.add_argument("biom_file",
                         help="BIOM file/OTU table.")
-    parser.add_argument("gramox_out",
-                        help="File to write out gramox data for each category.")
     parser.add_argument("mapping_file",
                         help="Mapping file.")
+    parser.add_argument("cat_gramox_out",
+                        help="File to write out percent abundance data for "
+                             "each category.")
+    parser.add_argument("sample_gramox_out",
+                        help="File to write out percent abundance data for "
+                             "each sample.")
     parser.add_argument("category", type=str,
                         help="Column name for sample categories.")
-    parser.add_argument("-c", "--colors", type=str, default=None,
-                        help="Column name for category colors. Default is husl"
-                             " color palette from seaborn library.")
+    parser.add_argument("-a", "--annotate_bars", action="store_true",
+                        help="If provided, each bar will be annotated with "
+                             "percent abundance (Y-axis value) Default is no "
+                             "annotation.")
+    parser.add_argument("-c", "--per_sid_abd",
+                        help="If provided, intermediate calculation values for "
+                             "per sample percent abundance will be written to "
+                             "this file.")
     parser.add_argument("-s", "--save_fig",
                         help="Gramox data plot will be saved into this file.")
     return parser.parse_args()
@@ -110,14 +122,10 @@ def main():
 
     # Read mapping file for category knowledge
     mapf = pd.read_csv(args.mapping_file, sep="\t")
-    if args.colors is not None:
-        cat_colors = defaultdict()
     cond = defaultdict()
     for rows in mapf.iterrows():
         row = rows[1]
         cond[row["#SampleID"]] = row[args.category]
-        if args.colors is not None:
-            cat_colors[row[args.category]] = row[args.colors]
 
     # Get relative abundances from biom file
     biomf = biom.load_table(args.biom_file)
@@ -132,32 +140,46 @@ def main():
             raise RuntimeError("OTU list from biom file doesn't match with "
                                "gramox data file list.")
 
-    # Initialize dict to collect gramox abundance data
+    # Initialize dict to collect gramox abundance data by category and sample
     cat = defaultdict(dict)
+    sid_gramox_abd = defaultdict(dict)
     for k in cond.keys():
         cat[cond[k]]["Unknown"] = 0
-        cat[cond[k]]["Gram Positive Aerobe"] = 0
+        cat[cond[k]]["Gram Positive Facultatives"] = 0
         cat[cond[k]]["Gram Positive Anaerobe"] = 0
-        cat[cond[k]]["Gram Negative Aerobe"] = 0
+        cat[cond[k]]["Gram Negative Facultatives"] = 0
         cat[cond[k]]["Gram Negative Anaerobe"] = 0
         cat[cond[k]]["Total"] = 0
+        sid_gramox_abd[k]["Unknown"] = 0
+        sid_gramox_abd[k]["Gram Positive Facultatives"] = 0
+        sid_gramox_abd[k]["Gram Positive Anaerobe"] = 0
+        sid_gramox_abd[k]["Gram Negative Facultatives"] = 0
+        sid_gramox_abd[k]["Gram Negative Anaerobe"] = 0
+        sid_gramox_abd[k]["Total"] = 0
 
     for sid, d in rel_abd.iteritems():
         for otu, abd in d.iteritems():
             cat[cond[sid]]["Total"] += abd
+            sid_gramox_abd[sid]["Total"] += abd
             if gramox_data[otu] == ["1", "1"]:
-                cat[cond[sid]]["Gram Positive Aerobe"] += abd
+                cat[cond[sid]]["Gram Positive Facultatives"] += abd
+                sid_gramox_abd[sid]["Gram Positive Facultatives"] += abd
             elif gramox_data[otu] == ["1", "0"]:
                 cat[cond[sid]]["Gram Positive Anaerobe"] += abd
+                sid_gramox_abd[sid]["Gram Positive Anaerobe"] += abd
             elif gramox_data[otu] == ["0", "1"]:
-                cat[cond[sid]]["Gram Negative Aerobe"] += abd
+                cat[cond[sid]]["Gram Negative Facultatives"] += abd
+                sid_gramox_abd[sid]["Gram Negative Facultatives"] += abd
             elif gramox_data[otu] == ["0", "0"]:
                 cat[cond[sid]]["Gram Negative Anaerobe"] += abd
+                sid_gramox_abd[sid]["Gram Negative Anaerobe"] += abd
             elif "NA" in gramox_data[otu]:
                 cat[cond[sid]]["Unknown"] += abd
+                sid_gramox_abd[sid]["Unknown"] += abd
 
     # Calculate and collect percent abundance data.
     pct_cat = defaultdict(dict)
+    pct_sid = defaultdict(dict)
     for condition, v in cat.iteritems():
         for gramox, abd in v.iteritems():
             tot_abd = cat[condition][gramox]
@@ -165,8 +187,15 @@ def main():
             if gramox != "Total":
                 pct_cat[condition][gramox] = tot_abd/total*100
 
+    for sampleid, v1 in sid_gramox_abd.iteritems():
+        for gramox1, abd in v1.iteritems():
+            tot_abd1 = sid_gramox_abd[sampleid][gramox1]
+            total1 = sid_gramox_abd[sampleid]["Total"]
+            if sid_gramox_abd != "Total":
+                pct_sid[sampleid][gramox1] = tot_abd1/total1*100
+
     # Write out the categorized gramox data
-    with open(args.gramox_out, "w") as kyu:
+    with open(args.cat_gramox_out, "w") as kyu:
         kyu.write("condition\tgramox\tabd\ttotal_abd\tpct_abd\n")
         for cond, val in cat.iteritems():
             for gramox in sorted(val.keys()):
@@ -175,28 +204,47 @@ def main():
                               cat[cond][gramox], cat[cond]["Total"],
                               pct_cat[cond][gramox]))
 
+    # Write out gramox data for each sample
+    with open(args.sample_gramox_out, "w") as kyu:
+        kyu.write("Sample\tGram Positive Facultatives\tGram Positive Anaerobe"
+                  "\tGram Negative Facultatives\tGram Negative Anaerobe\t"
+                  "Unknown\n")
+        for sid, val in sid_gramox_abd.iteritems():
+            kyu.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(sid,
+                      pct_sid[sid]["Gram Positive Facultatives"],
+                      pct_sid[sid]["Gram Positive Anaerobe"],
+                      pct_sid[sid]["Gram Negative Facultatives"],
+                      pct_sid[sid]["Gram Negative Anaerobe"],
+                      pct_sid[sid]["Unknown"],))
+    if args.per_sid_abd:
+        with open(args.per_sid_abd, "w") as qwe:
+            for gramox in sorted(val.keys()):
+                if gramox != "Total":
+                    qwe.write("{}\t{}\t{}\t{}\t{}\n".format(sid, gramox,
+                              sid_gramox_abd[sid][gramox],
+                              sid_gramox_abd[sid]["Total"],
+                              pct_sid[sid][gramox]))
+
     # Plot data
-    plot_data = pd.read_csv(args.gramox_out, sep="\t")
-    cat_order = sorted(np.unique(plot_data.condition))
-    if args.colors is not None:
-        palette_col = [cat_colors[c] for c in cat_order]
-    else:
-        palette_col = sns.set_palette("husl")
+    plot_data = pd.read_csv(args.cat_gramox_out, sep="\t")
+    plot_data = plot_data.sort_values("condition")
+    palette_col = ["#253494", "#2c7fb8", "#41b6c4", "#a1dab4", "#ffffcc"]
+    sns.set(style="whitegrid")
     fig = plt.figure(figsize=(20, 15))
     ax = sns.barplot(
-        x="gramox", y="pct_abd", hue="condition", data=plot_data,
-        hue_order=cat_order, palette=palette_col, saturation=1)
-    for p in ax.patches:
-        ax.annotate(np.round(p.get_height(), decimals=2),
-                    (p.get_x()+p.get_width()/2., p.get_height()), ha='center',
-                    va='bottom', xytext=(0, 5), textcoords='offset points',
-                    size=50*p.get_width())
+        x="condition", y="pct_abd", hue="gramox", data=plot_data,
+        hue_order=sorted(np.unique(plot_data.gramox)), palette=palette_col,
+        saturation=1)
+    if args.annotate_bars:
+        for p in ax.patches:
+            ax.annotate(np.round(p.get_height(), decimals=2),
+                        (p.get_x()+p.get_width()/2., p.get_height()), ha='center',
+                        va='bottom', xytext=(0, 5), textcoords='offset points',
+                        size=80*p.get_width())
     ax.set_xlabel("")
-    ax.set_xticklabels(np.unique(plot_data.gramox), size=18, alpha=1)
+    ax.set_xticklabels(np.unique(plot_data.condition), size=18, alpha=1)
     ax.set_ylabel("Abundance Percentage (%)", labelpad=20, size=18, alpha=1)
     ax.set_ylim(top=int(max(plot_data.pct_abd) + 5))
-    ax.set_yticklabels(range(0, int(max(plot_data.pct_abd) + 5), 10),
-                       alpha=1, size=18)
     leg = ax.legend(loc=0, fontsize=18, frameon=True)
     leg.get_frame().set_linewidth(1)
     leg.get_frame().set_edgecolor('k')
