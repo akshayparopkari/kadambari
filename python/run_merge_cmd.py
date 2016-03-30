ce16a38950072a97784631d41f9b86579f41e05d
Akshay Paropkari <paropkari.1@gmail.com>
2016-03-30 17:26:48 -0400

    Updated run_merge_cmd.py. Improved clarity on imports and directory name print out. Added explicit assignment of zipped R2 files.

--- python/run_merge_cmd.py
+++ python/run_merge_cmd.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python
 
-'''
+"""
 Abstract: Iterate through all folders and execute FLASh command.
 
 Date: 08/06/2015
 
 Author: Akshay Paropkari
-'''
+"""
 
-import os
-import subprocess as sp
+import sys
 import argparse
+from os import chdir, walk
+import subprocess as sp
+from os.path import join, relpath
 try:
     import shlex
 except ImportError as ie:
-    sys.exit('Please install {} module before executing this script.'\
+    sys.exit("Please install {} module before executing this script."
              .format(ie))
 
 
 def prog_options():
     parser = argparse.ArgumentParser(
-                description='Iterate through each folder in a file and run '
-                            'FLASh command on zipped Read 1 and Read 2 fastq '
-                            'files.')
-    parser.add_argument('sample_dir',
-                        help='Directory containing sample folders.')
+                description="Iterate through each folder in a file and run "
+                            "FLASh command on zipped Read 1 and Read 2 fastq "
+                            "files.")
+    parser.add_argument("sample_dir",
+                        help="Directory containing sample folders.")
     return parser.parse_args()
 
 
 def main():
     args = prog_options()
 
-    for root, dirs, files in os.walk(args.sample_dir):
+    for root, dirs, files in walk(args.sample_dir):
         if root != args.sample_dir:
-            os.chdir(root)
-            print root[24:27]
+            chdir(root)
+            print root.split("/")[-1]
 
             for file in files:
-                if file.endswith('R1_001.fastq.gz'):
+                if file.endswith("R1_001.fastq.gz"):
                     R1 = file
-                else:
+                elif file.endswith("R2_001.fastq.gz"):
                     R2 = file
-            in1 = 'flash {} {} -M 300 --cap-mismatch-quals'.format(R1, R2)
+            in1 = "flash {} {} -M 300 --cap-mismatch-quals".format(R1, R2)
             kwargs = shlex.split(in1)
+            print kwargs
             out = sp.check_output(kwargs)
             print out
-            with open('flash.txt', 'w') as outlog:
-                outlog.write('{}'.format(out))
+            with open("flash_log.txt", "w") as outlog:
+                outlog.write("{}".format(out))
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
