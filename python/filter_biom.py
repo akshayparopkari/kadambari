0c357536cf51ce25c7d239da4de8c301365e60ca
Akshay Paropkari <paropkari.1@gmail.com>
2016-07-01 16:44:30 -0400

    Updated filter_biom.py. Script is now Python3 compatible. Also added default value for --filter_otuids_fnh parameter.

--- python/filter_biom.py
+++ python/filter_biom.py
@@ -19,7 +19,7 @@ except ImportError as ie:
     importerrors.append(ie)
 if len(importerrors) > 0:
     for err in importerrors:
-        print "Import Error: {}".format(err)
+        print("Import Error: {}".format(err))
     sys.exit()
 
 
@@ -35,10 +35,12 @@ def handle_program_options():
                         help="Mapping file with sampleIDs to retain in it. The"
                              " '#SampleID' column will be used to get the list"
                              " of all ids to retain.")
-    parser.add_argument("-fo", "--filter_otuids_fnh",
+    parser.add_argument("-fo", "--filter_otuids_fnh", default="./otuids_to_discard.txt",
                         help="Path to file to write out the list of OTUIDs not present "
                              "in any SampleIDs in mapping file. This output is usually "
-                             "used to filter out unwanted otuids from .tre file.")
+                             "used to filter out unwanted otuids from .tre file. If not "
+                             "given, the discarded OTUIDs list will be saved in the "
+                             "current working directory.")
     return parser.parse_args()
 
 
@@ -58,18 +60,19 @@ def main():
     # Get filtered biom data
     keep_sampleIDs = list(mapf["#SampleID"])
     sid_filtered_biomf = biomf.filter(keep_sampleIDs, inplace=False)
-    print "{} sampleIDs retained from original biom file.".format(len(keep_sampleIDs))
+    print("\n{} sampleIDs retained from original biom file.".format(len(keep_sampleIDs)))
     obs_abd_sum = sid_filtered_biomf.sum(axis="observation")
     otuids = sid_filtered_biomf.ids("observation")
     abd_sum = {a: b for a, b in zip(otuids, obs_abd_sum)}
 
     # Run checks for redundant otus
-    redundant_otuids = [otu for otu, abd in abd_sum.iteritems() if abd == 0]
+    redundant_otuids = [otu for otu, abd in abd_sum.items() if abd == 0]
     otuid_filtered_biomf = sid_filtered_biomf.filter(redundant_otuids,
                                                      "observation",
                                                      invert=True,
                                                      inplace=False)
-    print "{} otuIDs filtered out of the original biom file.\n".format(len(redundant_otuids))
+    print("{} otuIDs filtered out of the original biom file.\n"
+          .format(len(redundant_otuids)))
 
     # Write out files
     with bo(args.output_biom_fnh, "w") as rth:
@@ -77,6 +80,7 @@ def main():
     with open(args.filter_otuids_fnh, "w") as yui:
         for otuid in redundant_otuids:
             yui.write("{}\n".format(otuid))
+
 
 if __name__ == "__main__":
     sys.exit(main())
