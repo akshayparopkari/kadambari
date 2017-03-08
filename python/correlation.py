74bbfe11cfa2c1dd998cbf126dc1cea36c02cc6d
Akshay Paropkari <paropkari.1@gmail.com>
2017-03-08 17:10:08 -0500

    Added new script
    Added script which calculates correlations for OTUs/genes based on biom
    file and mapping file. This script uses multiprocessing library, which
    will slow local machines. Testing for this script will follow.

--- python/correlation.py
+++ python/correlation.py
@@ -7,70 +7,96 @@
 
 import sys
 import argparse
-from itertools import combinations
-from phylotoast.otu_calc import otu_name
+from traceback import format_exc
+from time import localtime, strftime
+from itertools import combinations, izip
 from phylotoast.util import parse_map_file, gather_categories
 from phylotoast.biom_calc import arcsine_sqrt_transform as ast, relative_abundance
 importerrors = set()
 try:
     import biom
-except:
+except ImportError:
     importerrors.add("biom")
 try:
     import numpy as np
-except:
+except ImportError:
     importerrors.add("numpy")
 try:
-    from scipy.stats import rankdata, spearmanr as Spearman, kendalltau as KendallTau
-except ImportError as ie:
-    importerrors.add("scipy")
+    from multiprocessing import Pool, current_process
+except ImportError:
+    importerrors.add("multiprocessing")
 try:
-    from statsmodels.sandbox.stats.multicomp import multipletests as MT
-except:
+    from statsmodels.sandbox.stats.multicomp import fdrcorrection0 as FDR
+except ImportError:
     importerrors.add("statsmodels")
+try:
+    from scipy.stats import rankdata, spearmanr as Spearman, kendalltau as KendallTau
+except ImportError:
+    importerrors.add("scipy")
 if len(importerrors) > 0:
     for err in importerrors:
-        print("Please install missing module: {}".format(ie))
+        print("Please install missing module: {}".format(err))
     sys.exit()
 
 
-def calc_corr(biom_file, abd_data, otu_combos, gather_categories, method):
-    """
-    This function calculates the pairwaise spearman correlation for each group.
-    """
+def calc_corr(otu_combos, gather_categories, abd_data):
+    """This function calculates the pairwise spearman correlation for each group."""
+    starttime = strftime("%x | %X".format(localtime))
+    print("{0} | Core-{1}".format(starttime, current_process().name.split("-")[1]))
     result = []
     for combo in otu_combos:
-        a = []
-        b = []
-        otu1 = otu_name(biom_file.metadata(combo[0], "observation")["taxonomy"])
-        otu2 = otu_name(biom_file.metadata(combo[1], "observation")["taxonomy"])
+        list1 = []
+        list2 = []
+        otu1 = combo[0]
+        otu2 = combo[1]
         for cat in gather_categories.keys():
             for sid in gather_categories[cat].sids:
-                a.append(abd_data[sid][combo[0]])
-                b.append(abd_data[sid][combo[1]])
-            rho_corr, rho_p_val = Spearman(rankdata(a, "ordinal"), rankdata(b, "ordinal"))
-            kendalltau_corr, kendalltau_p_val = KendallTau(rankdata(a, "ordinal"),
-                                                           rankdata(b, "ordinal"))
-            result.append([cat, otu1, otu2, rho_corr, rho_p_val])
+                list1.append(abd_data[sid][otu1])
+                list2.append(abd_data[sid][otu2])
+        try:
+            assert len(list1) == len(list2)
+        except AssertionError:
+            sys.exit("Abundance array lengths do not match.")
+        rho_corr, rho_p_val = Spearman(rankdata(list1, "ordinal"),
+                                       rankdata(list2, "ordinal"))
+        result.append(["Spearman", cat, otu1, otu2, rho_corr, rho_p_val])
+        kendall_corr, kendall_p_val = KendallTau(rankdata(list1, "ordinal"),
+                                                 rankdata(list2, "ordinal"))
+        result.append(["Kendall", cat, otu1, otu2, kendall_corr, kendall_p_val])
+    return result
 
-    # Perform multiple testing correction
-    p_vals = [entry[-1] for entry in result]
-    hypothesis, pvals_corrected, alphacSidak, alphacBonf = MT(p_vals, method=method)
-    idx_to_retain = np.where(np.array(pvals_corrected) < 0.05)[0].tolist()
-    print("{} correlations removed through multiple test correction."
-          .format(len(result) - len(idx_to_retain)))
+
+def calc_corr_helper(args):
+    """Helper function to use Pool.map() one-argument worker functions."""
+    return calc_corr(*args)
+
+
+def run_fdr(result_list):
+    """Run BH FDR correction on correlation results."""
+    try:
+        assert all(isinstance(x, list) for x in result_list) is True
+    except AssertionError:
+        sys.exit("Data format for FDR Correction not compatible. Please check.")
+    else:
+        p_vals = [entry[-1] for entry in result_list]
+        hypothesis, pvals_corrected = FDR(p_vals)
+        idx_to_retain = np.where(np.array(pvals_corrected) < 0.05)[0].tolist()
+        print("{} correlations removed through multiple test correction."
+              .format(len(result_list) - len(idx_to_retain)))
     updated_result = []
     for idx in idx_to_retain:
         updated_p_val = pvals_corrected[idx]
-        updated_result.append((result[idx][0], result[idx][1], result[idx][2],
-                               result[idx][3], updated_p_val,))
+        updated_result.append((result_list[idx][0], result_list[idx][1],
+                               result_list[idx][2], result_list[idx][3],
+                               result_list[idx][4], updated_p_val,))
     return updated_result
 
 
 def program_options():
+    """Function to house the user inputs."""
     parser = argparse.ArgumentParser(
-        description=("Given an abundance table, calculate pairwise Spearman's Rho of the "
-                     "columns. Results undergo multiple testing correction (BH)."))
+        description=("Given an abundance table, calculate and return FDR corrected "
+                     "significant pairwise Spearman's Rho."))
     parser.add_argument("in_biomf", help="Input abundance file containing OTU names as "
                         "columns and SampleIDs as rows. Ideally, this is the output from "
                         "biom_relative_abundance.py script.")
@@ -80,13 +106,6 @@ def program_options():
     parser.add_argument("out_fnh", help="Path and name to output correlation matrix file."
                         " The format for the tab-separated file will be: Category -> "
                         "Variable -> by Variable -> Correlation")
-    parser.add_argument("-mt", "--multiple_test_method", default="fdr_bh",
-                        choices={"bonferroni", "holm", "fdr_bh", "fdr_by"},
-                        help="Method to be used for testing and adjusting p-values. By "
-                        "default, it is set to Benjamini/Hochberg. Please refer to http:/"
-                        "/statsmodels.sourceforge.net/stable/generated/statsmodels."
-                        "sandbox.stats.multicomp.multipletests.html for additional "
-                        "information.")
     return parser.parse_args()
 
 
@@ -97,27 +116,64 @@ def main():
         biomf = biom.load_table(args.in_biomf)
     except IOError as ioe:
         sys.exit("Error with input BIOM format file: {}".format(ioe))
-    rel_abd = relative_abundance(biomf)
-    ast_rel_abd = ast(rel_abd)
+    else:
+        rel_abd = relative_abundance(biomf)
+        ast_rel_abd = ast(rel_abd)
+        # Get pairwise combinations of OTUs
+        otu_combos = list(combinations(biomf.ids("observation"), 2))
 
     try:
         mheader, mdata = parse_map_file(args.map_fnh)
     except IOError as ioe:
         sys.exit("Error with input mapping file: {}".format(ioe))
+    else:
+        # Gather sampleID categories
+        sid_cat = gather_categories(mdata, mheader, [args.category_column])
 
-    # Gather sampleID categories
-    sid_cat = gather_categories(mdata, mheader, [args.category_column])
+    # Create arguments for helper function to be supplied to multiprocessing pool.map()
+    chunksize = 1000
+    jobs = [(otu_combos[x:x+chunksize], sid_cat, ast_rel_abd,)
+            for x in xrange(0, len(otu_combos), chunksize)]
+    print("{0} jobs created.".format(len(jobs)))
 
-    # Get pairwise combinations of OTUs
-    otu_combos = combinations(biomf.ids("observation"), 2)
+    # Start multiprocessing jobs
+    try:
+        print("Starting map_async()...")
+        pool = Pool()
+        res = pool.map_async(calc_corr_helper, jobs)
+        pool.close()
+        pool.join()
+    except Exception:
+        sys.exit("Error while calculating correlations\n{}".format(format_exc()))
+    else:
+        s_rho_calc = []
+        k_tau_calc = []
+        for r in res.get():
+            for s in r:
+                if s[0] == "Spearman":
+                    s_rho_calc.append(s)
+                else:
+                    k_tau_calc.append(s)
 
-    # Get correlation calculation results
-    corr_results = calc_corr(biomf, ast_rel_abd, otu_combos, sid_cat,
-                             args.multiple_test_method)
+    # Get FDR corrected correlation results
+    print("Running FDR correction on {} Spearman's Rho.".format(len(s_rho_calc)))
+    fdr_corr_s_rho = run_fdr(s_rho_calc)
+    print("Running FDR correction on {} Kendall Tau.".format(len(k_tau_calc)))
+    fdr_corr_k_tau = run_fdr(k_tau_calc)
 
+    # Consolidate correlation results
+    k_kos = {(e[2], e[3],) for e in fdr_corr_k_tau}
+    s_kos = {(f[2], f[3],) for f in fdr_corr_s_rho}
+    final_kos = s_kos & k_kos
+    print("{0} elements in k_kos\n{1} elements in s_kos\n{2} elements are common to both."
+          .format(len(k_kos), len(s_kos), len(final_kos)))
+    final_fdr_corr_results = [cdata[1:] for cdata in fdr_corr_s_rho
+                              if (cdata[2], cdata[3],) in final_kos]
+
+    # Write our results to file
     with open(args.out_fnh, "w") as outf:
         outf.write("Category\tVariable\tby Variable\tCorrelation\tp value\n")
-        for k in corr_results:
+        for k in final_fdr_corr_results:
             outf.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(k[0], k[1], k[2], k[3], k[4]))
 
 
