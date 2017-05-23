dc39dc95cb05ee1e6f9779f7d4b87e95d7d6fd70
Akshay Paropkari <paropkari.1@gmail.com>
2017-05-23 13:03:54 -0400

    Updated gramox_calc.py
    - Updated imports and added checks for ImportError. This script has
    phylotoast dependency.
    - Deprecated genus-level OTU gramox calculations.
    - Added user directive for transparent transition to using
    pct_abd_gramox.py script.

--- python/gramox_calc.py
+++ python/gramox_calc.py
@@ -1,66 +1,66 @@
 #!/usr/bin/env python
 """
-Abstract: Calculate gram stain and oxygen requirement (gramox) for all OTU
-          from their relative abundance file and write the output to file.
-
-Date: 06/29/2015
-
+Abstract: Calculate gram stain and oxygen requirement (gramox) for all OTU from BIOM
+          format file.
 Author: Akshay Paropkari
+Date: 06/29/2015
 """
-import argparse
 import sys
+import argparse
+importerrors = []
 try:
-    import pandas as pd
+    import biom
 except ImportError:
-    sys.exit("Please install pandas package.")
+    importerrors.append("biom")
+try:
+    from phylotoast.otu_calc import otu_name
+except ImportError:
+    importerrors.append("phylotoast")
+if len(importerrors) > 0:
+    for err in importerrors:
+        print("Please install missing package: {}".format(err))
+    sys.exit()
 
 
 def handle_program_options():
-    parser = argparse.ArgumentParser(
-        description="Get gramox data for OTU's from relative abundance file."
-    )
-    parser.add_argument("master_fnh",
-                        help="Path to master gramox data file.")
-    parser.add_argument("rel_abd_fnh",
-                        help="Path to relative abundance data file.")
-    parser.add_argument("out_fnh",
-                        help="Path to output file.")
-    parser.add_argument("-l", "--taxa_lvl", default="s", choices=["s", "g"],
-                        help="Path to relative abundance data file.")
+    parser = argparse.ArgumentParser(description="Get gram stain and oxygen requirement "
+                                     "(gramox) data for s-OTUs from a given BIOM file. "
+                                     "Gram Positives are 1, Gram Negatives are 0. "
+                                     "Obligate Anaerobes are 0 and rest are classified as"
+                                     " Aerobes with 1.")
+    parser.add_argument("master_fnh", help="Path to master gramox data file [REQUIRED]")
+    parser.add_argument("biom_file", help="BIOM file/OTU table. This file is used to "
+                        "collect the list of OTUs for processing their gramox data "
+                        "[REQUIRED]")
+    parser.add_argument("out_fnh", help="Path to output file [REQUIRED]")
     return parser.parse_args()
 
 
 def main():
     args = handle_program_options()
 
+    # Read gramox data from master file
     try:
-        with open(args.master_fnh):
-            pass
-    except IOError as ioe:
-        err_msg = "\nError opening master gramox file: {}\n"
-        sys.exit(err_msg.format(ioe))
-
-    try:
-        with open(args.rel_abd_fnh):
-            pass
-    except IOError as ioe:
-        err_msg = "\nError opening relative abundance file: {}\n"
-        sys.exit(err_msg.format(ioe))
-
-# Read relative abundance data
-    rel_abd_data = pd.read_csv(args.rel_abd_fnh, sep="\t")
-    otus = rel_abd_data["#OTU ID"]
-
-# Read gramox data from master file
-    with open(args.master_fnh, "rU") as bgof:
-        if args.taxa_lvl == "g":
-            bgodata = {line.strip().split("\t")[0]: "\t".join(line.strip().split("\t")[2:4])
-                       for line in bgof.readlines()}
-        else:
+        with open(args.master_fnh, "rU") as bgof:
             bgodata = {line.strip().split("\t")[1]: "\t".join(line.strip().split("\t")[2:4])
                        for line in bgof.readlines()}
+    except Exception as err:
+        sys.exit("\nError parsing master gramox file: {}. Please check master gramox data"
+                 " file and re-run this script.".format(err))
 
-# Write classified gramox data to tsv file
+    # Read relative abundance data
+    try:
+        biomf = biom.load_table(args.biom_file)
+    except Exception as err:
+        sys.exit("\nError opening BIOM file: {}\n".format(err))
+    else:
+        otus = [otu_name(biomf.metadata(otuid, "observation")["taxonomy"])
+                for otuid in biomf.ids("observation")]
+
+    # Write classified gramox data to tsv file
+    print("\nWriting out results to file. For OTUs with missing gramox data, please "
+          "manually input the relevant information or fill in NA for unverified "
+          "information. This is required before running pct_abd_gramox.py script.\n")
     with open(args.out_fnh, "w") as gramoxout:
         gramoxout.write("#OTU\tGram Status\tOxygen Requirement\n")
         for otu in otus:
@@ -69,5 +69,6 @@ def main():
             else:
                 gramoxout.write("{}\n".format(otu))
 
+
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
